99a3c3a3f09940fef448f0c05227796a120de41e
shikin.a.v <shikin.a.v@mail.ru>
2021-12-07 20:17:25 +0300

    publishMyModel

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,7 @@ exports.saveMyModelPart = require('./constructor/saveMyModelPart')
 exports.saveMyModel = require('./constructor/saveMyModel')
 exports.deleteMyModel = require('./constructor/deleteMyModel')
 
+exports.publishMyModel = require('./constructor/publishMyModel')
+
 exports.loadStoryPage = require('./constructor/loadStoryPage')
 exports.loadStoryModel = require('./constructor/loadStoryModel')
