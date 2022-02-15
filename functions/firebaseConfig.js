a772006c98f32a2d251e9ac24d9fd7bf2425894b
shikin.a.v <shikin.a.v@mail.ru>
2022-02-16 00:38:33 +0400

    correct firebase env variables

--- functions/firebaseConfig.js
+++ functions/firebaseConfig.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
 module.exports = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: process.env.FIRE_AUTH_DOMAIN,
+  databaseURL: process.env.FIRE_DATABASE_URL,
+  projectId: process.env.FIRE_PROJECT_ID,
+  storageBucket: process.env.FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRE_APP_ID,
+  measurementId: process.env.FIRE_MEASUREMENT_ID,
 }
