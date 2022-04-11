b8b14f23fc8e66b4c3d2a6925b783c09a99a23dc
shikin.a.v <shikin.a.v@mail.ru>
2022-04-11 23:42:22 +0400

    constructor constants; step.lastPlayedModelId

---
+++ functions/constructor/constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  LIMIT_NEED_PUBLISH_MODELS: 1000,
+
+  LOAD_TYPE: {
+    LAST: 'last',
+    PREVIOUS: 'previous',
+  },
+  
+  STEP_STATUS: {
+    NEW: 'new',
+    PROGRESS: 'progress',
+    COMPLETE: 'complete',
+  }
+}
